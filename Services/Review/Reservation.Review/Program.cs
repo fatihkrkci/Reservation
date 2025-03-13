bdd058e7c177749f5e57831137990c2747ee48f8
Fatih Kürekçi <fatihkr02@gmail.com>
2025-03-13 22:44:34 +0300

    completed Review microservice and created IdentityServer microservice

--- Services/Review/Reservation.Review/Program.cs
+++ Services/Review/Reservation.Review/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Reservation.Review.Context;
+using Reservation.Review.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ReviewContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserReviewService, UserReviewService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
