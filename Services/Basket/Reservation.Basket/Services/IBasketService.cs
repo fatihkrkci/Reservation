a5cb2b58cf61e2cc38750a06075c5293d30863cf
Fatih Kürekçi <fatihkr02@gmail.com>
2025-03-27 22:36:16 +0300

    created Basket microservice

---
+++ Services/Basket/Reservation.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using Reservation.Basket.Dtos;
+
+namespace Reservation.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasket(string userId);
+        Task SaveBasket(BasketTotalDto basketTotalDto);
+        Task DeleteBasket(string userId);
+    }
+}
